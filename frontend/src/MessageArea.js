553ac7cdd760c07de9bd27d46ebd677e46de6d8a
RasheMpofu <68470142+RasheMpofu@users.noreply.github.com>
2026-06-26 22:12:19 +0200

    Update MessageArea.js

--- frontend/src/MessageArea.js
+++ frontend/src/MessageArea.js
@@ -1,16 +1,14 @@
+
 import React, {useEffect, useRef} from "react";
 import './MessageArea.css'
 import {Message} from "./Message";
 
 function MessageArea(props) {
 
-    /*
-      Autoscrolling
-     */
     const messagesEndRef = useRef(null)
 
     useEffect(() => {
-        //scroll to bottom when a message is sent or received
+     
         if (props.messages.length > 1) {
             scrollToBottom();
         }
@@ -20,13 +18,24 @@ function MessageArea(props) {
         messagesEndRef.current.scrollIntoView({behavior: "smooth"})
     }
 
-    return (
-        <ul id="messages">
-            {props.messages.map((item, i) =>
-                (<Message text={item.text} position={item.position} timestamp={item.timestamp}/>))}
-            <li ref={messagesEndRef}/>
-        </ul>
-    )
+  return (
+    <ul id="messages">
+        {props.messages.map((item, i) =>
+            (<Message text={item.text} position={item.position} timestamp={item.timestamp}/>))}
+        {props.isTyping && (
+            <li className="typing-indicator">
+                <div className="avatar"/>
+                <div className="typing-dots">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </li>
+        )}
+        <li ref={messagesEndRef}/>
+    </ul>
+)
+    
 }
 
 export {MessageArea}
