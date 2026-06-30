b6a1cf176a96eb63d5be66499eefe5a240c37396
RasheMpofu <68470142+RasheMpofu@users.noreply.github.com>
2026-06-30 15:03:22 +0200

    Update Message.js

--- frontend/src/Message.js
+++ frontend/src/Message.js
@@ -1,40 +1,21 @@
-import React, {useEffect, useRef} from "react";
-import './MessageArea.css'
-import {Message} from "./Message";
+import React from "react";
+import './Message.css'
 
-function MessageArea(props) {
+function Message(props) {
 
-    const messagesEndRef = useRef(null)
-
-    useEffect(() => {
-     
-        if (props.messages.length > 1) {
-            scrollToBottom();
-        }
-    })
-
-    function scrollToBottom() {
-        messagesEndRef.current.scrollIntoView({behavior: "smooth"})
-    }
-
-  return (
-    <ul id="messages">
-        {props.messages.map((item, i) =>
-            (<Message text={item.text} position={item.position} timestamp={item.timestamp}/>))}
-        {props.isTyping && (
-            <li className="typing-indicator">
-                <div className="avatar"/>
-                <div className="typing-dots">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-            </li>
-        )}
-        <li ref={messagesEndRef}/>
-    </ul>
-)
-    
+    return (
+        <li className={"message appeared " + props.position}>
+            {props.position === "left" ? (
+    <div className="avatar">🤖</div>
+) : (
+    <div className="avatar">👤</div>
+)}
+            <div className="text_wrapper">
+                <div className="text">{props.text}</div>
+                <div className="timestamp">{props.timestamp}</div>
+            </div>
+        </li>
+    )
 }
 
-export {MessageArea}
+export {Message}
