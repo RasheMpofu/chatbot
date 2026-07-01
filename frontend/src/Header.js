bae167461712fdc45170272037fb6fd1cd6493bf
RasheMpofu <68470142+RasheMpofu@users.noreply.github.com>
2026-07-01 18:20:42 +0200

    Update Header.js

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -8,7 +8,10 @@ function Header(props) {
                 <div className="button minimize"/>
                 <div className="button maximize"/>
             </div>
-            <div className="title">ORDER HERE!</div>
+        <div className="title">
+              <div style={{fontSize: '30px', fontWeight: '700'}}>🍕 PizzaHutBot</div>
+              <div style={{fontSize: '13px', fontWeight: '300', opacity: '0.8', marginTop: '4px'}}>● Online</div>
+        </div>
             <button onClick={props.onReset} className="reset_button">Reset </button>
              <button onClick={props.onExport} className="export_button">Export </button>
         </div>
